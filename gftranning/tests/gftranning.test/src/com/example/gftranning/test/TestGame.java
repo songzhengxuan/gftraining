0de511c6b14008329e2456b61914b245f37579d8
songzhengxuan <songzhengxuan@360.cn>
2014-04-08 21:34:30 +0800

    add test project

---
+++ gftranning/tests/gftranning.test/src/com/example/gftranning/test/TestGame.java
@@ -0,0 +1,11 @@
+package com.example.gftranning.test;
+
+import android.test.AndroidTestCase;
+
+public class TestGame extends AndroidTestCase {
+
+    public void testGame() {
+        assertEquals(true, false);
+    }
+
+}
